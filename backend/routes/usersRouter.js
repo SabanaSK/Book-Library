c4b3f3a8a0d6ca627c2bd12e8932958032b7e7d5
SabanaSK <Sornkarn_su@hotmail.com>
2023-10-21 18:15:57 +0200

    Added verifyAdmin to getAllUsers and Delete users

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import usersControllers from "../controllers/usersControllers.js";
+import verifyAdmin from "../middleware/verify.js";
 
 const router = express.Router();
 
-router.post("/login", usersControllers.login);  
+router.post("/login", usersControllers.login);
 router.post("/autoLogin", usersControllers.autoLogin);
 
 //These two might be delete or edit
-router.get("/", usersControllers.getAllUsers);
-router.delete("/:id", usersControllers.deleteById);
+router.get("/", verifyAdmin, usersControllers.getAllUsers);
+router.delete("/:id", verifyAdmin, usersControllers.deleteById);
 
 export default router;
