3bcc26edd282d48f7a34a23ac11e8e68c51065ea
SabanaSK <Sornkarn_su@hotmail.com>
2023-11-13 13:43:52 +0100

    style user and usersTable

--- src/components/UsersComponent/User/User.jsx
+++ src/components/UsersComponent/User/User.jsx
@@ -1,15 +1,26 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./User.module.css";
 
 const User = ({ user }) => {
   return (
-    <div>
-      <p>Username: {user.username}</p>
-      <p>email: {user.email}</p>
-      <p>status: {user.status}</p>
-      <p>Role: {user.role}</p>
-      <Link to={`/edit-user/${user.id}`}>
-        <button>Edit</button>
+    <div className={styles["user-container"]}>
+      <p className={styles["user-description"]}>Username:</p>
+      <p className={styles["user-detail"]}>{user.username}</p>
+      <p className={styles["user-description"]}>Email:</p>
+      <p className={styles["user-detail"]}>{user.email}</p>
+      <p className={styles["user-description"]}>CreatedAt:</p>
+      <p className={styles["user-detail"]}>{user.createdAt}</p>
+      <p className={styles["user-description"]}>UpdatedBy:</p>
+      <p className={styles["user-detail"]}>{user.updatedBy}</p>
+      <p className={styles["user-description"]}>UpdatedAt:</p>
+      <p className={styles["user-detail"]}>{user.updatedAt}</p>
+      <p className={styles["user-description"]}>Status:</p>
+      <p className={styles["user-detail"]}>{user.status}</p>
+      <p className={styles["user-description"]}>Role:</p>
+      <p className={styles["user-detail"]}>{user.role}</p>
+      <Link to={`/edit-user/${user.id}`} className={styles["edit-link"]}>
+        <button className={styles["edit-button"]}>Edit</button>
       </Link>
     </div>
   );
@@ -20,6 +31,9 @@ User.propTypes = {
     id: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    updatedBy: PropTypes.string.isRequired,
+    updatedAt: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
     role: PropTypes.string.isRequired,
   }).isRequired,
--- src/components/UsersComponent/UsersTable/UsersTable.jsx
+++ src/components/UsersComponent/UsersTable/UsersTable.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../../services/userServices";
 import User from "../User/User";
+import styles from "./UsersTable.module.css";
 
 const UsersTable = () => {
   const [users, setUsers] = useState([]);
@@ -16,7 +17,7 @@ const UsersTable = () => {
       });
   }, []);
   return (
-    <div>
+    <div className={styles["users-container"]}>
       {users.map((user) => (
         <User key={user.id} user={user} />
       ))}
